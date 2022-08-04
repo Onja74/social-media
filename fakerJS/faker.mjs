a85741865dfd4e8fa2129f016cfabe12bd0e2dc7
Onja74 <hei.fitahiana@gmail.com>
2022-08-04 12:02:50 +0100

    upload_fackerJS

--- fakerJS/faker.mjs
+++ fakerJS/faker.mjs
@@ -1,9 +1,22 @@
 import { faker } from '@faker-js/faker';
 import fs from "fs";
+
 let data ="INSERT INTO account VALUES";
 
-let firstName = faker.name.firstName().replace('\'', '');
-   let lastName = faker.name.lastName().replace('\'', '');
+const number = 250_000;
+
+for(let i =1; i<=number; i++){
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
+
+    let nickname = faker.name.findName();
+    let birthday = faker.date.betweens('1930-01-01','2004-01-01',1)[0].toISOString().split('T')[0]; 
+    let email = faker.internet.email();
+    let gender = faker.name.gender(true);
+    let profile_pic = firstName+".png";
+        data+= `\n (${i},'${firstName.replace('\'','')}','${lastName.replace('\'', '')}','${nickname.replace('\'', '')}','${birthday}','${gender}','${email}','${profile_pic.replace('\'', '')}')`  
+        i+1<number? data+=",":data+=";";
+    }
 
 fs.writeFile('resultat.sql', data, (err)=>{
     if(err){
